4b2e6f6ab364e03f988a5cbcd24e5db09fb8dab6
Ocholla-T <45341560+Ocholla-T@users.noreply.github.com>
2019-06-20 08:09:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
